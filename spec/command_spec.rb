2370c4d26459a9498899b54f9ecb92c0b5cad9c0
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-06-22 11:31:26 -0500

    Command spec refactor

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -5,31 +5,29 @@ class TestInstance
 end
 
 describe GitHandler::GitCommand do
-  before do
-    @obj = TestInstance.new
-  end
+  let(:command) { TestInstance.new }
 
   it 'detects a valid git command' do
-    @obj.valid_command?("invalid command").should be_false
-    @obj.valid_command?("line1\nline2").should be_false
-    @obj.valid_command?("git-receive-pack").should be_false
-    @obj.valid_command?("git-receive-pack repo.git").should be_false
-    @obj.valid_command?("git-receive-pack 'repo'").should be_true
-    @obj.valid_command?("git-receive-pack 'repo.git'").should be_true
-    @obj.valid_command?("git-receive-pack 'repo'\ngit-upload-pack 'repo'").should be_false
+    command.valid_command?("invalid command").should be_false
+    command.valid_command?("line1\nline2").should be_false
+    command.valid_command?("git-receive-pack").should be_false
+    command.valid_command?("git-receive-pack repo.git").should be_false
+    command.valid_command?("git-receive-pack 'repo'").should be_true
+    command.valid_command?("git-receive-pack 'repo.git'").should be_true
+    command.valid_command?("git-receive-pack 'repo'\ngit-upload-pack 'repo'").should be_false
   end
 
   context '.parse_command' do
     it 'raises error on invalid git command' do
-      proc { @obj.parse_command("invalid command") }.
+      proc { command.parse_command("invalid command") }.
         should raise_error GitHandler::ParseError
 
-      proc { @obj.parse_command("git-receive-pack 'repo.git'") }.
+      proc { command.parse_command("git-receive-pack 'repo.git'") }.
         should_not raise_error GitHandler::ParseError
     end
 
     it 'returns a proper action and repo' do
-      result = @obj.parse_command("git-receive-pack 'repo.git'")
+      result = command.parse_command("git-receive-pack 'repo.git'")
       result.should be_a Hash
       result.should eql(
         :action => 'git-receive-pack', 
@@ -41,17 +39,17 @@ describe GitHandler::GitCommand do
   end
 
   it 'detects read command' do
-    @obj.read_command?('git-receive-pack').should be_false
-    @obj.read_command?('git-upload-pack').should be_true
-    @obj.read_command?('git upload-pack').should be_true
-    @obj.read_command?('git-upload-archive').should be_true
-    @obj.read_command?('git upload-archive').should be_true
+    command.read_command?('git-receive-pack').should be_false
+    command.read_command?('git-upload-pack').should be_true
+    command.read_command?('git upload-pack').should be_true
+    command.read_command?('git-upload-archive').should be_true
+    command.read_command?('git upload-archive').should be_true
   end
 
   it 'detects write command' do
-    @obj.write_command?("git-upload-pack").should be_false
-    @obj.write_command?("git-upload-archive").should be_false
-    @obj.write_command?("git receive-pack").should be_true
-    @obj.write_command?("git-receive-pack").should be_true
+    command.write_command?("git-upload-pack").should be_false
+    command.write_command?("git-upload-archive").should be_false
+    command.write_command?("git receive-pack").should be_true
+    command.write_command?("git-receive-pack").should be_true
   end
 end
