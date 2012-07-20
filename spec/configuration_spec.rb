0b200bb091f457b36e8f6a05481f850378152c4b
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-07-19 20:30:14 -0500

    Check configuration defaults

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -7,5 +7,6 @@ describe GitHandler::Configuration do
     config.home_path.should eq('/home/git')
     config.repos_path.should eq('/home/git/repositories')
     config.log_path.should eq('/home/git/access.log')
+    config.raise_errors.should eq(true)
   end
 end
