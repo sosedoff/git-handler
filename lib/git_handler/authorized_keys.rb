b4c62f86a9259f007e454a2e49c5b87cebc98cbe
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-10-20 19:22:52 -0500

    Refactor authorized keys module

--- lib/git_handler/authorized_keys.rb
+++ lib/git_handler/authorized_keys.rb
@@ -1,8 +1,27 @@
 module GitHandler
   module AuthorizedKeys
+    # Write formatted keys content to file
+    # @param path [String] path path to authorized_keys file
+    # @param keys [Array<PublicKey>] public key objects
+    # @param command [String] custom command for the key
+    def self.write_keys(path, keys, command)
+      content = keys.map { |k| k.to_system_key(command) }.join("\n").strip
+      self.write(path, content)
+    end
+
+    # Write a single key formatted content to file
+    # @param path [String] authorized keys file path
+    # @param key [PublicKey] public key object
+    # @param command [String] custom command for the key
+    def self.write_key(path, key, command)
+      self.write_keys(path, [key], command)
+    end
+
+    protected
+
     # Write contents to file with lock
-    # @param [String] path path to output file
-    # @param [String] content key buffer
+    # @param path [String] output file path
+    # @param content [String] key content
     def self.write(path, content)
       raise ArgumentError, "File \"#{path}\" does not exist."  if !File.exists?(path)
       raise ArgumentError, "File \"#{path}\" is not writable." if !File.writable?(path)  
@@ -12,23 +31,6 @@ module GitHandler
         f.write(content)
         f.flock(File::LOCK_UN)
       end
-    end
-
-    # Write formatted keys content to file
-    # @param [String] path path to authorized_keys file
-    # @param [Array<PublicKey>] keys public keys array
-    # @command [String] command custom command for the key
-    def self.write_keys(path, keys, command)
-      content = keys.map { |k| k.to_system_key(command) }.join("\n").strip
-      self.write(path, content)
-    end
-
-    # Write a single key formatted content to file
-    # @param [String] path path to authorized_keys file
-    # @param [PublicKey] keys public key instance
-    # @command [String] command custom command for the key
-    def self.write_key(path, key, command)
-      self.write_keys(path, [key], command)
     end
   end
 end
